9bacd7d4cef1d921f08379891a57c4c3c6c09e3a
strongbase55 <strongbase55@gmail.com>
2024-02-24 00:19:40 +0530

    supper

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,14 @@
  * add event on element
  */
 
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+const addEventOnElem = function(elem, type, callback) {
+    if (elem.length > 1) {
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
+    } else {
+        elem.addEventListener(type, callback);
     }
-  } else {
-    elem.addEventListener(type, callback);
-  }
 }
 
 
@@ -26,18 +26,18 @@ const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-  document.body.classList.toggle("active");
+const toggleNavbar = function() {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+    document.body.classList.toggle("active");
 }
 
 addEventOnElem(navToggler, "click", toggleNavbar);
 
-const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-  document.body.classList.remove("active");
+const closeNavbar = function() {
+    navbar.classList.remove("active");
+    navToggler.classList.remove("active");
+    document.body.classList.remove("active");
 }
 
 addEventOnElem(navbarLinks, "click", closeNavbar);
@@ -50,12 +50,12 @@ addEventOnElem(navbarLinks, "click", closeNavbar);
 
 const header = document.querySelector("[data-header]");
 
-const activeHeader = function () {
-  if (window.scrollY > 300) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
+const activeHeader = function() {
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
 }
 
 addEventOnElem(window, "scroll", activeHeader);
@@ -68,8 +68,8 @@ addEventOnElem(window, "scroll", activeHeader);
 
 const addToFavBtns = document.querySelectorAll("[data-add-to-fav]");
 
-const toggleActive = function () {
-  this.classList.toggle("active");
+const toggleActive = function() {
+    this.classList.toggle("active");
 }
 
 addEventOnElem(addToFavBtns, "click", toggleActive);
@@ -82,14 +82,14 @@ addEventOnElem(addToFavBtns, "click", toggleActive);
 
 const sections = document.querySelectorAll("[data-section]");
 
-const scrollReveal = function () {
-  for (let i = 0; i < sections.length; i++) {
-    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
-      sections[i].classList.add("active");
-    } else {
-      sections[i].classList.remove("active");
+const scrollReveal = function() {
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+            sections[i].classList.add("active");
+        } else {
+            sections[i].classList.remove("active");
+        }
     }
-  }
 }
 
 scrollReveal();
